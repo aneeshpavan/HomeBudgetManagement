34fb562c1caf33f04a356478d8271ff906963287
Aneesh Pavan Prodduturu <43541169+aneeshpavan@users.noreply.github.com>
2020-04-20 14:39:39 +0530

    Update HomeBudgetManagementDemo.java

--- HomeBudgetManagementDemo.java
+++ HomeBudgetManagementDemo.java
@@ -1,13 +1,14 @@
 import java.util.*;
-interface HomeDemo
+interface HomeDemo//An interface named HomeDemo
 {
-    public void setPrices();
-    public void showPrices();
-    public void compareToGivenAmount(int amount);
+    public void setPrices();//Abstract method which will be overridden in future in child classes
+    public void showPrices();//Abstract method to show prices
+    public void compareToGivenAmount(int amount);//Abstract method for comparison
 }
 
-abstract class Home implements HomeDemo
+abstract class Home implements HomeDemo // class Home which implements the interface HomeDemo
 {
+    //data members to store values
     public  int w;
     public  int defaultAmount=12000;
     public  int vegetables = 1500;
@@ -16,14 +17,14 @@ abstract class Home implements HomeDemo
     public  int ElectricityBill = 600;
     public  int Waterbill = 600;
     public  int Rent = 5000;
-    public  void showPrices()
+    public  void showPrices()//member method to show prices
     {
         System.out.println("\tYour Budget is : "+defaultAmount);
         String s="\t\t\t\tvegetables Price : "+this.vegetables+"\n\t\t\t\tMilkEggs Price : "+this.MilkEggs+"\n\t\t\t\tMedicines Price : "+this.Medicines+"\n\t\t\t\tElectricity Bill : "+this.ElectricityBill+"\n\t\t\t\tWater Bill "+this.Waterbill+"\n\t\t\t\tRent : "+this.Rent;
         System.out.println(s);
         System.out.println("\n\n");
     }
-    public  void compareToGivenAmount(int amount)
+    public  void compareToGivenAmount(int amount)//member method for comparison
     {
         if(amount>this.defaultAmount){
             System.out.println("\t\t\t\tOUT OF BUDGET");
@@ -35,9 +36,9 @@ abstract class Home implements HomeDemo
     }
 }
 
-class HomeBudgetManagementDemo extends Home implements HomeDemo
+class HomeBudgetManagementDemo extends Home implements HomeDemo //Class HomeBudgetManagementDemo which inherits Home and implements HomeDemo. It is the subclass of Home
 {
-    public  void setPrices()
+    public  void setPrices()//Member method to set prices.
     {
         int a,b,c,d,e,f;
         Scanner sc=new Scanner(System.in);
@@ -64,25 +65,25 @@ class HomeBudgetManagementDemo extends Home implements HomeDemo
         
     }
 
-    public static void main(String[] args)
+    public static void main(String[] args)//Main function
     {
         HomeBudgetManagementDemo h=new HomeBudgetManagementDemo();
         Scanner sc=new Scanner(System.in);
     
-    while(true)
+    while(true)//Infinite while loop to keep menu visible
     {
         System.out.println("\t\t\t1.Show Prices\n\t\t\t2.Set Prices\n\t\t\t3.Comparing OUT OF BUDGET/IN BUDGET? \n\t\t\t4.EXIT\nEnter your option : ");
-        int input=sc.nextInt();
+        int input=sc.nextInt();//input for choosing option
         if(input==1)
-            h.showPrices();
+            h.showPrices();//method showPrices is invoked using the object of HomeBudgetManagementDemo
         else if(input==2)
-            h.setPrices();
+            h.setPrices();//method setPrices is invoked using the object of HomeBudgetManagementDemo
         else if(input==3)
         {
             
             System.out.println("enter the sum of your budget amount : ");
             int amt=sc.nextInt();
-            h.compareToGivenAmount(amt);
+            h.compareToGivenAmount(amt);//method compareToGivenAmount(amt) is invoked using the object of HomeBudgetManagementDemo
         }
         else
             return ;
